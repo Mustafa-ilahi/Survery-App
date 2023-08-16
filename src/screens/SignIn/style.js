7530ece3a636c5aa2aea107b581ce8274b408cf2
Simation Studios <simationstudios@Simations-MacBook-Pro.local>
2023-08-16 17:59:25 +0500

    sign in updated

--- src/screens/SignIn/style.js
+++ src/screens/SignIn/style.js
@@ -21,17 +21,48 @@ export const styles = StyleSheet.create({
     fontSize: fontSize.h6,
   },
   inputTop: {
-    marginTop: sizes.screenHeight * 0.03,
+    marginTop: sizes.screenHeight * 0.02,
+    marginBottom: sizes.screenHeight * 0.01,
   },
   input: {
     backgroundColor: 'transparent',
-    // borderWidth: 1,
     width: sizes.screenWidth * 0.9,
     alignSelf: 'center',
-    // borderRadius: sizes.screenWidth * 0.03,
-    // borderTopRightRadius: sizes.screenWidth * 0.03,
-    // borderTopLeftRadius: sizes.screenWidth * 0.03,
-    // borderColor: colors.grayBorder,
+    borderRadius: sizes.screenWidth * 0.03,
+    borderTopRightRadius: sizes.screenWidth * 0.03,
+    borderTopLeftRadius: sizes.screenWidth * 0.03,
+    borderColor: colors.grayBorder,
   },
   container: {height: sizes.screenHeight, backgroundColor: colors.white},
+  signInBtn: {
+    height: sizes.screenHeight * 0.07,
+    width: sizes.screenWidth * 0.93,
+    alignSelf: 'center',
+  },
+  btnText: {
+    textAlign: 'center',
+    marginTop: sizes.screenHeight * 0.015,
+    color: '#EFEFF0',
+    fontSize: fontSize.medium,
+  },
+  forgetView: {
+    alignItems: 'center',
+  },
+  forgetText: {
+    fontSize: fontSize.medium,
+    color: '#61646B',
+  },
+  row: {
+    flexDirection: 'row',
+    alignSelf: 'center',
+  },
+  bottom: {
+    marginTop: sizes.screenHeight * 0.19,
+  },
+  signUpText: {
+    fontSize: fontSize.medium,
+    color: colors.appTextColor1,
+    fontWeight: '800',
+    left: sizes.screenWidth * 0.01,
+  },
 });
--- src/services/utilities/images/index.js
+++ src/services/utilities/images/index.js
@@ -3,11 +3,13 @@ import profile from '../../../assets/Profile.png';
 import facebook from '../../../assets/facebook.png';
 import apple from '../../../assets/apple.png';
 import google from '../../../assets/google.png';
+import buttonBg from '../../../assets/buttonBg.png';
 const images = {
   crossIcon,
   profile,
   facebook,
   apple,
-  google
+  google,
+  buttonBg
 };
 export default images;
