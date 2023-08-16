00297824e2d91ea8d3ac93aa6aa6ad05902b1e6d
Simation Studios <simationstudios@Simations-MacBook-Pro.local>
2023-08-16 12:31:12 +0500

    folder structure

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SafeAreaView,View, Text} from 'react-native'
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Test</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
