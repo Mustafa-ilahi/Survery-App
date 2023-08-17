28e8290bf5d789cc74fc265f84a47107cca4eebb
Simation Studios <simationstudios@Simations-MacBook-Pro.local>
2023-08-17 12:55:45 +0500

    otp screen created

--- App.js
+++ App.js
@@ -4,13 +4,15 @@ import LandingPage from './src/screens/LandingPage'
 import SignIn from './src/screens/SignIn'
 import SignUp from './src/screens/SignUp'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
+import OTP from './src/screens/OTP'
 
 export default function App() {
   return (
     <SafeAreaProvider>
       {/* <LandingPage /> */}
       {/* <SignIn /> */}
-      <SignUp />
+      {/* <SignUp /> */}
+      <OTP />
     </SafeAreaProvider>
   )
 }
---
+++ src/screens/OTP/index.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import {styles} from './style';
+import {SafeAreaView, View, Text, TouchableOpacity} from 'react-native';
+import CrossBtn from '../../components/CrossBtn';
+
+import {
+    CodeField,
+    Cursor,
+    useBlurOnFulfill,
+    useClearByFocusCell,
+  } from 'react-native-confirmation-code-field';
+  
+export default function OTP() {
+    const [value, setValue] = useState('');
+
+    const ref = useBlurOnFulfill({value, cellCount: CELL_COUNT});
+    const [props, getCellOnLayoutHandler] = useClearByFocusCell({
+      value,
+      setValue,
+    });
+    const CELL_COUNT = 6;
+
+  return (
+    <SafeAreaView>
+      <View style={styles.container}>
+        <View>
+          <CrossBtn />
+        </View>
+        <View style={styles.content}>
+          <Text style={styles.otpHead}>Enter OTP</Text>
+          <View style={styles.textView}>
+            <Text style={styles.text}>
+              You would’ve received an OTP on your phone number *** *** 239
+            </Text>
+          </View>
+        </View>
+
+        <View style={styles.padding}>
+          <CodeField
+            ref={ref}
+            {...props}
+            // Use `caretHidden={false}` when users can't paste a text value, because context menu doesn't appear
+            value={value}
+            onChangeText={setValue}
+            cellCount={CELL_COUNT}
+            rootStyle={styles.codeFieldRoot}
+            keyboardType="number-pad"
+            textContentType="oneTimeCode"
+            renderCell={({index, symbol, isFocused}) => (
+              <Text
+                key={index}
+                style={[styles.cell, isFocused && styles.focusCell]}
+                onLayout={getCellOnLayoutHandler(index)}>
+                {symbol || (isFocused ? <Cursor /> : null)}
+              </Text>
+            )}
+          />
+        </View>
+
+        <View style={[styles.row, styles.bottom]}>
+          <Text style={styles.forgetText}>Didn’t receive the OTP? </Text>
+          <TouchableOpacity>
+            <Text style={styles.resendText}>Resend</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={[styles.row, styles.footerbottom]}>
+          <Text style={styles.forgetText}>Not your number? </Text>
+          <TouchableOpacity>
+            <Text style={styles.forgetText}>Change your number</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
